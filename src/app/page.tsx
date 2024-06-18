d21f10c4a1049d2beecaa1b3294afde78ff50729
Poglia <pedro.poglia@hotmail.com>
2024-06-17 22:34:17 -0300

    feat: Create QuizGame in react/next/tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,68 @@
-import { StudentTable } from "@/components/StudentTable";
-import { students } from "@/data/students";
+"use client";
 
-export default function Page() {
+import Button from "@/components/Button";
+import { QuestionItem } from "@/components/QuestionItem";
+import { Results } from "@/components/Results";
+import { questions } from "@/data/questions";
+import { useState } from "react";
+
+const Page = () => {
+  const [answers, setAnswers] = useState<number[]>([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const title = "Quiz do Poglia";
+
+  const loadNextQuestion = () => {
+    if (questions[currentQuestion + 1]) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  const handleStartButton = () => {
+    setAnswers([]);
+    setCurrentQuestion(0);
+    setShowResult(false);
+  }
+
+  const handleAnswered = (answer: number) => {
+    setAnswers([...answers, answer]);
+
+    loadNextQuestion();
+  };
   return (
-    <div className=" text-white">
-      <h1 className="text-5xl mb-">Lista de estudantes</h1>
-      <StudentTable students={students}></StudentTable>
+    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+      <div className="w-full max-w-xl rounded-md bg-white text-black shadow-black">
+        <div className="p-5 font-bold text-2xl border-b border-gray-300">
+          {title}
+        </div>
+        <div className="p-5">
+          {!showResult && (
+            <QuestionItem
+              question={questions[currentQuestion]}
+              count={currentQuestion + 1}
+              onAnswer={handleAnswered}
+            />
+          )}
+          {showResult && <Results questions={questions} answers={answers} />}
+        </div>
+        <div className="p-5 text-center border-t border-gray-300">
+          {!showResult &&
+            `
+          ${currentQuestion + 1} de ${questions.length} pergunta${
+              questions.length === 1 ? "" : "s"
+            }
+          `}
+          {showResult && (
+            <button onClick={handleStartButton}className="px-3 py-2 rounded-md bg-blue-800 text-white">
+              Reiniciar Quiz
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
---
+++ src/data/questions.ts
@@ -0,0 +1,19 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+  {
+    question: "Quem é o meu melhor amigo?",
+    options: ["Mauro", "Zanella", "Renan", "Vithor"],
+    answer: 0,
+  },
+  {
+    question: "Qual é a minha bebida favorita",
+    options: ["Coca", "Suco de laranja", "Mate", "Agua"],
+    answer: 2,
+  },
+  {
+    question: "Qual é o meu time do coração",
+    options: ["Juventude", "Caxias", "Gremio", "Internacional"],
+    answer: 3,
+  },
+];
